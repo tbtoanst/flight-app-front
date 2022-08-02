f795139dd37080a54c3392eb18bade760a311373
DESKTOP-F892L4I\Tauhu <tbtoanst@gmail.com>
2022-08-02 23:48:24 +0700

    added flight component

---
+++ src/app/flights/flights.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlightsComponent } from './flights.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    FlightsComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[],
+  providers:[]
+})
+export class FlightsModule { }
